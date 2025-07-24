5abadc1fec88c4dacab2f66b9e98e3d57662fba9
Zzz1511619zzZ <Zzz1511619zzZ@gmail.com>
2025-07-24 23:09:19 +0900

    feat: Add svelte-forms-lib dependency and implement cart item count display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { expoIn } from 'svelte/easing';
+	import { Cart, IsCartOpen } from '../stores/Cart.js';
+	import CartDetails from '$lib/components/Cart/CartDetails.svelte';
+	import { scale } from 'svelte/transition';
 	import ProductList from '$lib/components/ProductList/ProductList.svelte';
 	import { t } from '$lib/i18n/i18n.js';
 	//import Main from './Main.svelte';
@@ -7,6 +11,8 @@
 	export let data: { props: { products: { tags: string[] }[] } };
 	const products = data.props.products;
 	const bestseller = products.filter((product: { tags: string[] }) => product.tags.includes('bestseller'));
+	let y = 0;
+	$: cartNumber = $Cart.reduce((count, item) => count + item.qty, 0);
 </script>
 
 <svelte:head>
@@ -24,9 +30,47 @@
 		</span>
 	</h1>
 
+	
 	<div class="text-center">
 		<div class="pt-8">
 			<div class="text-2xl">{$t('home.bestseller_title')}</div>
+				<span
+					class="hidden w-0 right-0 bg-lightGray absolute ease-out duration-100 transition-all rounded-full -z-50 md:flex group-hover:w-full group-hover:h-full"
+				/>
+				<div
+					class="hidden invisible text-2xl px-2 {y < 90
+						? 'text-white'
+						: 'text-black'} md:flex group-hover:visible"
+				>
+					{$t('navbar.shopping_cart')}
+				</div>
+				<!-- Cart Icon -->
+				<svg
+					in:scale|local={{ duration: 200, start: 1.5, opacity: 1, easing: expoIn }}
+					class="block h-12 w-12 {y < 90 ? 'bg-white' : 'bg-black'} rounded-full p-2 ml-auto"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke={y < 90 ? 'black' : 'white'}
+					stroke-width="2"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+					/></svg
+				>
+				<!-- Cart Items count -->
+				{#key cartNumber}
+					<div
+						class="flex rounded-full text-2sm fixed {y < 90
+							? 'bg-white text-black'
+							: 'bg-black'} items-center top-12 right-10 px-2"
+						in:scale={{ duration: 200, start: 1.5, opacity: 1, easing: expoIn }}
+					>
+						{cartNumber}
+					</div>
+				{/key}
 			<ProductList products={bestseller} />
 		</div>
 	</div>
